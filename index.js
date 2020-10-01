4e09d6b8be0a029fc17a0dd69b5293c38ff127f3
concoctionfactory <david.g.lee.nyc@gmail.com>
2020-10-01 19:18:13 -0400

    changed local host to 5000

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   log.error(err);
   res.status(err.status || 500);
   res.json({
@@ -38,9 +38,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
   log.info(
     'Express server listening on http://localhost:%d',
     server.address().port
